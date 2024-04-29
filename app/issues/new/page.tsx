ed10eb680704ba670ad036ec1b37d9b7a7fa80c7
jotish03 <jotishsharma03@gmail.com>
2024-04-29 21:26:05 +0530

    added react-simplemde-editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+"use client";
+
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return (
     <div className="flex justify-center">
       <main className="flex flex-col items-center justify-center gap-2 w-1/2 mt-12">
         <Input placeholder="Enter Issue Title" />
-        <Textarea placeholder="Enter Issue Description" />
+        <SimpleMDE placeholder="Enter Issue Description" className="w-full " />
         <Button className="w-full">Submit Issue</Button>
       </main>
     </div>
