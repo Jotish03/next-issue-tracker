f8187fcf71eed65ac51f56779a53620dfd2dc097
Jotish Sharma <jotishsharma03@gmail.com>
2024-04-30 13:53:51 +0530

    react-hook-form added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,17 +2,48 @@
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssue = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="flex justify-center">
-      <main className="flex flex-col items-center justify-center gap-2 w-1/2 mt-12">
-        <Input placeholder="Enter Issue Title" />
-        <SimpleMDE placeholder="Enter Issue Description" className="w-full " />
+    <div
+      className="flex justify-center"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issue", data);
+        router.push("/issues");
+      })}
+    >
+      <form className="flex flex-col items-center justify-center gap-2 w-1/2 mt-12">
+        <Input
+          placeholder="Enter Issue Title"
+          {...register("title", { required: true })}
+        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              placeholder="Enter Issue Description"
+              className="w-full "
+              {...field}
+            />
+          )}
+        />
+
         <Button className="w-full">Submit Issue</Button>
-      </main>
+      </form>
     </div>
   );
 };
