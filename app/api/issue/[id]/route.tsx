498c4f91cdc27924d4e96003ba48ed688a4e146f
Jotish Sharma <jotishsharma03@gmail.com>
2024-05-08 00:21:08 +0530

    assignedToUserId api endpoint added

--- app/api/issue/[id]/route.tsx
+++ app/api/issue/[id]/route.tsx
@@ -1,6 +1,10 @@
 import { AuthOptions } from "@/components/auth-provider/auth-options";
-import { issueSchema } from "@/components/layout/validation-schema";
+import {
+  issueSchema,
+  patchIssueSchema,
+} from "@/components/layout/validation-schema";
 import prisma from "@/prisma/client";
+import { ObjectId } from "bson";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,19 +13,37 @@ interface Props {
 }
 
 export async function PATCH(request: NextRequest, { params: { id } }: Props) {
-  const session = await getServerSession(AuthOptions);
+  // const session = await getServerSession(AuthOptions);
 
-  if (!session)
-    return NextResponse.json("Not Allowed, Please Try Signing in again", {
-      status: 401,
-    });
+  // if (!session)
+  //   return NextResponse.json("Not Allowed, Please Try Signing in again", {
+  //     status: 401,
+  //   });
   const body = await request.json();
 
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 404 });
   }
 
+  const { assignedToUserId, title, description } = body;
+
+  // Check if assignedToUserId is provided and valid
+  if (assignedToUserId && ObjectId.isValid(assignedToUserId)) {
+    const objectId = new ObjectId(assignedToUserId);
+
+    const user = await prisma.user.findUnique({
+      where: { id: objectId.toString() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid User" }, { status: 400 });
+    }
+    // If the user exists, you can proceed with other operations
+  } else {
+    // Handle the case when assignedToUserId is not provided or invalid
+    return NextResponse.json({ error: "Error with ID" }, { status: 404 });
+  }
   const issue = await prisma.issue.findUnique({
     where: {
       id,
@@ -35,8 +57,9 @@ export async function PATCH(request: NextRequest, { params: { id } }: Props) {
   const updatedIssue = await prisma.issue.update({
     where: { id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
--- components/layout/validation-schema.tsx
+++ components/layout/validation-schema.tsx
@@ -2,5 +2,23 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string({ message: "Description fields is required!" }).min(1),
+  description: z
+    .string({ message: "Description fields is required!" })
+    .min(1)
+    .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1).max(255).optional(),
+  description: z
+    .string({ message: "Description fields is required!" })
+    .min(1)
+    .max(255)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
+    .max(65535)
+    .optional()
+    .nullable(),
 });
